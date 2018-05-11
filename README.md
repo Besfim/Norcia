f953b4af3e95c15069f09fa77985bdcea51e99ad
Ericwyn <Ericwyn.chen@gmail.com>
2018-05-11 18:24:30 +0800

    add 2 markdown file for test, update the config.json file

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Norcia
-A simple blog framework
+一个简单的静态博客框架
+
+## 项目结构
+ - 根目录下的 HTML \ CSS \ JavaScript 文件
+ - `norcia` 文件夹存放是用来维护 `config.json` 的一个程序
+ - `document` 文件夹用来存放博文 markdown 文件
+ - `document_mini` 文件夹用来存放博文的首部, 由 go 程序自动维护, 方便主页显示博文前200个字预览
--- js/script.js
+++ js/script.js
@@ -59,7 +59,7 @@ function domId(domId) {
  * 获取url中的参数
  */
 function getUrlParam(name) {
- var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
- var r = window.location.search.substr(1).match(reg); //匹配目标参数
+ let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+ let r = window.location.search.substr(1).match(reg); //匹配目标参数
  if (r != null) return unescape(r[2]); return null; //返回参数值
 }
